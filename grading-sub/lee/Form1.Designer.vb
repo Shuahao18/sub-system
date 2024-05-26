dcb37f38038765f9bbdb6660c42d48dd0660e6a0
watïiee <114126437+Shuahao18@users.noreply.github.com>
2024-05-26 09:16:21 -0700

    Add files via upload

---
+++ grading-sub/lee/Form1.Designer.vb
@@ -0,0 +1,545 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        TextBox11 = New TextBox()
+        TextBox12 = New TextBox()
+        TextBox13 = New TextBox()
+        TextBox14 = New TextBox()
+        TextBox15 = New TextBox()
+        TextBox16 = New TextBox()
+        TextBox17 = New TextBox()
+        TextBox18 = New TextBox()
+        TextBox19 = New TextBox()
+        TextBox20 = New TextBox()
+        TextBox21 = New TextBox()
+        TextBox22 = New TextBox()
+        TextBox23 = New TextBox()
+        TextBox24 = New TextBox()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        TextBox25 = New TextBox()
+        TextBox26 = New TextBox()
+        TextBox27 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox28 = New TextBox()
+        TextBox29 = New TextBox()
+        TextBox30 = New TextBox()
+        Button4 = New Button()
+        TextBox31 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(127, 415)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(164, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "COMPUTE PRELIM"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(45, 55)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(47, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "QUIZ#1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(45, 92)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(47, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "QUIZ#2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(45, 131)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(47, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "QUIZ#3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(45, 174)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(70, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "RECITATION"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(45, 209)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(80, 15)
+        Label5.TabIndex = 5
+        Label5.Text = "ATTENDANCE"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(45, 251)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(68, 15)
+        Label6.TabIndex = 6
+        Label6.Text = "ACTIVITY#1"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(45, 294)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(68, 15)
+        Label7.TabIndex = 7
+        Label7.Text = "ACTIVITY#2"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(45, 336)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(68, 15)
+        Label8.TabIndex = 8
+        Label8.Text = "ACTIVITY#3"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(45, 375)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(39, 15)
+        Label9.TabIndex = 9
+        Label9.Text = "EXAM"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(144, 47)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 10
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(144, 84)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 11
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(144, 123)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 12
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(144, 166)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 13
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(144, 201)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 14
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(144, 243)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 15
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(144, 286)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 16
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(144, 328)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(100, 23)
+        TextBox8.TabIndex = 17
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(366, 42)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(100, 23)
+        TextBox9.TabIndex = 18
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(366, 84)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(100, 23)
+        TextBox10.TabIndex = 19
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(366, 123)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(100, 23)
+        TextBox11.TabIndex = 20
+        ' 
+        ' TextBox12
+        ' 
+        TextBox12.Location = New Point(366, 166)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(100, 23)
+        TextBox12.TabIndex = 21
+        ' 
+        ' TextBox13
+        ' 
+        TextBox13.Location = New Point(366, 201)
+        TextBox13.Name = "TextBox13"
+        TextBox13.Size = New Size(100, 23)
+        TextBox13.TabIndex = 22
+        ' 
+        ' TextBox14
+        ' 
+        TextBox14.Location = New Point(366, 243)
+        TextBox14.Name = "TextBox14"
+        TextBox14.Size = New Size(100, 23)
+        TextBox14.TabIndex = 23
+        ' 
+        ' TextBox15
+        ' 
+        TextBox15.Location = New Point(366, 286)
+        TextBox15.Name = "TextBox15"
+        TextBox15.Size = New Size(100, 23)
+        TextBox15.TabIndex = 24
+        ' 
+        ' TextBox16
+        ' 
+        TextBox16.Location = New Point(366, 328)
+        TextBox16.Name = "TextBox16"
+        TextBox16.Size = New Size(100, 23)
+        TextBox16.TabIndex = 25
+        ' 
+        ' TextBox17
+        ' 
+        TextBox17.Location = New Point(564, 47)
+        TextBox17.Name = "TextBox17"
+        TextBox17.Size = New Size(100, 23)
+        TextBox17.TabIndex = 26
+        ' 
+        ' TextBox18
+        ' 
+        TextBox18.Location = New Point(564, 84)
+        TextBox18.Name = "TextBox18"
+        TextBox18.Size = New Size(100, 23)
+        TextBox18.TabIndex = 27
+        ' 
+        ' TextBox19
+        ' 
+        TextBox19.Location = New Point(564, 123)
+        TextBox19.Name = "TextBox19"
+        TextBox19.Size = New Size(100, 23)
+        TextBox19.TabIndex = 28
+        ' 
+        ' TextBox20
+        ' 
+        TextBox20.Location = New Point(564, 166)
+        TextBox20.Name = "TextBox20"
+        TextBox20.Size = New Size(100, 23)
+        TextBox20.TabIndex = 29
+        ' 
+        ' TextBox21
+        ' 
+        TextBox21.Location = New Point(564, 201)
+        TextBox21.Name = "TextBox21"
+        TextBox21.Size = New Size(100, 23)
+        TextBox21.TabIndex = 30
+        ' 
+        ' TextBox22
+        ' 
+        TextBox22.Location = New Point(564, 243)
+        TextBox22.Name = "TextBox22"
+        TextBox22.Size = New Size(100, 23)
+        TextBox22.TabIndex = 31
+        ' 
+        ' TextBox23
+        ' 
+        TextBox23.Location = New Point(564, 286)
+        TextBox23.Name = "TextBox23"
+        TextBox23.Size = New Size(100, 23)
+        TextBox23.TabIndex = 32
+        ' 
+        ' TextBox24
+        ' 
+        TextBox24.Location = New Point(564, 328)
+        TextBox24.Name = "TextBox24"
+        TextBox24.Size = New Size(100, 23)
+        TextBox24.TabIndex = 33
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label10.Location = New Point(157, 12)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(49, 15)
+        Label10.TabIndex = 34
+        Label10.Text = "PRELIM"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label11.Location = New Point(396, 13)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(63, 15)
+        Label11.TabIndex = 35
+        Label11.Text = "MIDTERM"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label12.Location = New Point(589, 13)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(47, 15)
+        Label12.TabIndex = 36
+        Label12.Text = "FINALS"
+        ' 
+        ' TextBox25
+        ' 
+        TextBox25.Location = New Point(144, 372)
+        TextBox25.Name = "TextBox25"
+        TextBox25.Size = New Size(100, 23)
+        TextBox25.TabIndex = 37
+        ' 
+        ' TextBox26
+        ' 
+        TextBox26.Location = New Point(366, 375)
+        TextBox26.Name = "TextBox26"
+        TextBox26.Size = New Size(100, 23)
+        TextBox26.TabIndex = 38
+        ' 
+        ' TextBox27
+        ' 
+        TextBox27.Location = New Point(564, 375)
+        TextBox27.Name = "TextBox27"
+        TextBox27.Size = New Size(100, 23)
+        TextBox27.TabIndex = 39
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(340, 415)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(164, 23)
+        Button2.TabIndex = 40
+        Button2.Text = "COMPUTE MIDTERM"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(533, 415)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(164, 23)
+        Button3.TabIndex = 41
+        Button3.Text = "COMPUTE FINALS"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox28
+        ' 
+        TextBox28.Location = New Point(144, 444)
+        TextBox28.Name = "TextBox28"
+        TextBox28.Size = New Size(100, 23)
+        TextBox28.TabIndex = 42
+        ' 
+        ' TextBox29
+        ' 
+        TextBox29.Location = New Point(366, 444)
+        TextBox29.Name = "TextBox29"
+        TextBox29.Size = New Size(100, 23)
+        TextBox29.TabIndex = 43
+        ' 
+        ' TextBox30
+        ' 
+        TextBox30.Location = New Point(564, 444)
+        TextBox30.Name = "TextBox30"
+        TextBox30.Size = New Size(100, 23)
+        TextBox30.TabIndex = 44
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(245, 482)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(115, 47)
+        Button4.TabIndex = 45
+        Button4.Text = "COMPUTE TOTAL GRADE"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox31
+        ' 
+        TextBox31.Location = New Point(366, 495)
+        TextBox31.Name = "TextBox31"
+        TextBox31.Size = New Size(100, 23)
+        TextBox31.TabIndex = 46
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(255))
+        ClientSize = New Size(720, 603)
+        Controls.Add(TextBox31)
+        Controls.Add(Button4)
+        Controls.Add(TextBox30)
+        Controls.Add(TextBox29)
+        Controls.Add(TextBox28)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox27)
+        Controls.Add(TextBox26)
+        Controls.Add(TextBox25)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(TextBox24)
+        Controls.Add(TextBox23)
+        Controls.Add(TextBox22)
+        Controls.Add(TextBox21)
+        Controls.Add(TextBox20)
+        Controls.Add(TextBox19)
+        Controls.Add(TextBox18)
+        Controls.Add(TextBox17)
+        Controls.Add(TextBox16)
+        Controls.Add(TextBox15)
+        Controls.Add(TextBox14)
+        Controls.Add(TextBox13)
+        Controls.Add(TextBox12)
+        Controls.Add(TextBox11)
+        Controls.Add(TextBox10)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents TextBox18 As TextBox
+    Friend WithEvents TextBox19 As TextBox
+    Friend WithEvents TextBox20 As TextBox
+    Friend WithEvents TextBox21 As TextBox
+    Friend WithEvents TextBox22 As TextBox
+    Friend WithEvents TextBox23 As TextBox
+    Friend WithEvents TextBox24 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox25 As TextBox
+    Friend WithEvents TextBox26 As TextBox
+    Friend WithEvents TextBox27 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox28 As TextBox
+    Friend WithEvents TextBox29 As TextBox
+    Friend WithEvents TextBox30 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox31 As TextBox
+
+End Class
---
+++ grading-sub/lee/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    Dim PQ1, PQ2, PQ3, PR, PA, PA1, PA2, PA3, PEX, PQT, PRT, PAT, PATT, PET, GG, GH, PT As Double
+    Dim MQ1, MQ2, MQ3, MR, MA, MA1, MA2, MA3, MEX, MQT, MRT, MAT, MATT, MET, HH, MT As Double
+    Dim FQ1, FQ2, FQ3, FR, FA, FA1, FA2, FA3, FEX, FQT, FRT, FAT, FATT, FET, LL, FT, total, TOTALGRADE As Double
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        PQ1 = Val(TextBox1.Text)
+        PQ2 = Val(TextBox2.Text)
+        PQ3 = Val(TextBox3.Text)
+        PR = Val(TextBox4.Text)
+        PA = Val(TextBox5.Text)
+        PA1 = Val(TextBox6.Text)
+        PA2 = Val(TextBox7.Text)
+        PA3 = Val(TextBox8.Text)
+        PEX = Val(TextBox25.Text)
+
+        PQT = (PQ1 + PQ2 + PQ3) / 3 * 0.2
+        PRT = PR * 0.15
+        PAT = PA * 0.1
+        PATT = (PA1 + PA2 + PA3) / 3 * 0.25
+        PET = PEX * 0.3
+        PT = (PQT + PRT + PAT + PATT + PET) * 0.3
+        GG = PT
+
+        TextBox28.Text = GG
+
+
+        MQ1 = Val(TextBox9.Text)
+        MQ2 = Val(TextBox10.Text)
+        MQ3 = Val(TextBox11.Text)
+        MR = Val(TextBox12.Text)
+        MA = Val(TextBox13.Text)
+        MA1 = Val(TextBox14.Text)
+        MA2 = Val(TextBox15.Text)
+        MA3 = Val(TextBox16.Text)
+        MEX = Val(TextBox26.Text)
+
+        MQT = (MQ1 + MQ2 + MQ3) / 3 * 0.2
+        MRT = MR * 0.15
+        MAT = MA * 0.1
+        MATT = (MA1 + MA2 + MA3) / 3 * 0.25
+        MET = MEX * 0.3
+        MT = (MQT + MRT + MAT + MATT + MET) * 0.3
+        HH = MT
+
+        TextBox29.Text = HH
+
+        FQ1 = Val(TextBox17.Text)
+        FQ2 = Val(TextBox18.Text)
+        FQ3 = Val(TextBox19.Text)
+        FR = Val(TextBox20.Text)
+        FA = Val(TextBox21.Text)
+        FA1 = Val(TextBox22.Text)
+        FA2 = Val(TextBox23.Text)
+        FA3 = Val(TextBox24.Text)
+        FEX = Val(TextBox27.Text)
+
+        FQT = (FQ1 + FQ2 + FQ3) / 3 * 0.2
+        FRT = FR * 0.15
+        FAT = FA * 0.1
+        FATT = (FA1 + FA2 + FA3) / 3 * 0.25
+        FET = FEX * 0.3
+        FT = (FQT + FRT + FAT + FATT + FET) * 0.3
+        LL = FT
+
+        TextBox30.Text = LL
+
+        total = LL + HH + GG
+
+        TextBox31.Text = total
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PQ1 = Val(TextBox1.Text)
+        PQ2 = Val(TextBox2.Text)
+        PQ3 = Val(TextBox3.Text)
+        PR = Val(TextBox4.Text)
+        PA = Val(TextBox5.Text)
+        PA1 = Val(TextBox6.Text)
+        PA2 = Val(TextBox7.Text)
+        PA3 = Val(TextBox8.Text)
+        PEX = Val(TextBox25.Text)
+
+        PQT = (PQ1 + PQ2 + PQ3) / 3 * 0.2
+        PRT = PR * 0.15
+        PAT = PA * 0.1
+        PATT = (PA1 + PA2 + PA3) / 3 * 0.25
+        PET = PEX * 0.3
+        PT = (PQT + PRT + PAT + PATT + PET) * 0.3
+        GG = PT
+
+        TextBox28.Text = GG
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        MQ1 = Val(TextBox9.Text)
+        MQ2 = Val(TextBox10.Text)
+        MQ3 = Val(TextBox11.Text)
+        MR = Val(TextBox12.Text)
+        MA = Val(TextBox13.Text)
+        MA1 = Val(TextBox14.Text)
+        MA2 = Val(TextBox15.Text)
+        MA3 = Val(TextBox16.Text)
+        MEX = Val(TextBox26.Text)
+
+        MQT = (MQ1 + MQ2 + MQ3) / 3 * 0.2
+        MRT = MR * 0.15
+        MAT = MA * 0.1
+        MATT = (MA1 + MA2 + MA3) / 3 * 0.25
+        MET = MEX * 0.3
+        MT = (MQT + MRT + MAT + MATT + MET) * 0.3
+        HH = MT
+
+        TextBox29.Text = HH
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        FQ1 = Val(TextBox17.Text)
+        FQ2 = Val(TextBox18.Text)
+        FQ3 = Val(TextBox19.Text)
+        FR = Val(TextBox20.Text)
+        FA = Val(TextBox21.Text)
+        FA1 = Val(TextBox22.Text)
+        FA2 = Val(TextBox23.Text)
+        FA3 = Val(TextBox24.Text)
+        FEX = Val(TextBox27.Text)
+
+        FQT = (FQ1 + FQ2 + FQ3) / 3 * 0.2
+        FRT = FR * 0.15
+        FAT = FA * 0.1
+        FATT = (FA1 + FA2 + FA3) / 3 * 0.25
+        FET = FEX * 0.3
+        FT = (FQT + FRT + FAT + FATT + FET) * 0.3
+        LL = FT
+
+        TextBox30.Text = LL
+    End Sub
+End Class
